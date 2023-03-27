d1573889c1915429f4d471b617cca37ab2829f89
Ayeza <ayezabashir442@gmail.com>
2023-03-27 15:57:52 +0500

    final commit

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-var item = document.getElementById('item');
+var item = document.querySelector('#item');
+var toDoBox = document.querySelector('#to-do-box');
+
+item.addEventListener('keyup', function (event) {
+    // console.log(event.key);
+
+    if (event.key == "Enter") {
+        addToDo(this.value);
+        this.value = "";
+    }
+
+})
+
+const addToDo = (item) => {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `
+    ${item}
+    <i class="ri-close-line"></i>
+    `;
+
+    listItem.addEventListener("click", function () {
+        this.classList.toggle("done");
+    })
+
+    listItem.querySelector("i").addEventListener("click", function () {
+        listItem.remove();
+    })
+    toDoBox.appendChild(listItem);
+}
