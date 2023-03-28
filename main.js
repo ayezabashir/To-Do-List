bdbaae4c5d786d5b6d1d409c4fcf704dd29b00ca
Ayeza <ayezabashir442@gmail.com>
2023-03-28 23:25:18 +0500

    adding tasks to local storage

--- main.js
+++ main.js
@@ -14,16 +14,38 @@ item.addEventListener('keyup', function (event) {
 const addToDo = (item) => {
     const listItem = document.createElement('li');
     listItem.innerHTML = `
-    ${item}
-    <i class="ri-close-line"></i>
+      ${item}
+      <i class="ri-close-line"></i>
     `;
 
-    listItem.addEventListener("click", function () {
-        this.classList.toggle("done");
-    })
+    listItem.addEventListener('click', function () {
+        this.classList.toggle('done');
+    });
 
-    listItem.querySelector("i").addEventListener("click", function () {
+    listItem.querySelector('i').addEventListener('click', function () {
         listItem.remove();
-    })
+        updateLocalStorage();
+    });
+
     toDoBox.appendChild(listItem);
+    updateLocalStorage();
+};
+
+const updateLocalStorage = () => {
+    const items = [];
+
+    // Loop through each list item and add its text content to the items array
+    toDoBox.querySelectorAll('li').forEach((item) => {
+        items.push(item.textContent.trim());
+    });
+
+    // Store the items array in local storage
+    localStorage.setItem('toDoItems', JSON.stringify(items));
+};
+
+const savedItems = JSON.parse(localStorage.getItem('toDoItems'));
+if (savedItems) {
+    savedItems.forEach((item) => {
+        addToDo(item);
+    });
 }
